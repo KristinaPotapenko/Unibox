bf54ec038387adafbf2bf4274a93f7eff1b3fa25
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-23 12:11:20 +0300

    Add base structure App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Footer } from "./react/components/layouts/Footer/Footer";
+import { Header } from "./react/components/layouts/Header/Header";
+import { AppRoutes } from "./Routes/AppRoutes";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <main>
+        <AppRoutes />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
